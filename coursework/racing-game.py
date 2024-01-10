b828fc2c0fac878441bdae229e63ca4e5f2bbf44
Luca123456789123 <154456827+Luca123456789123@users.noreply.github.com>
2024-01-10 09:54:48 +0000

    Update racing-game.py

--- coursework/racing-game.py
+++ coursework/racing-game.py
@@ -79,6 +79,22 @@ clock = pygame.time.Clock()
 images = [(GRASS, (0, 0)), (TRACK, (0, 0))]
 player_car = PlayerCar(4, 4)
 
+
+def run():
+   
+GRASS = scale_image(pygame.image.load("imgs/grass.jpg"), 2.5)
+TRACK = scale_image(pygame.image.load("imgs/track.png"), 0.9)
+
+TRACK_BORDER = scale_image(pygame.image.load("imgs/track-border.png"), 0.9)
+
+RED_CAR = scale_image(pygame.image.load("imgs/red-car.png"), 0.55)
+GREEN_CAR = scale_image(pygame.image.load("imgs/green-car.png"), 0.55)
+
+WIDTH, HEIGHT = TRACK.get_width(), TRACK.get_height()
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Racing Game!")
+
+FPS = 60
 while run:
     clock.tick(FPS)
 
@@ -102,6 +118,7 @@ while run:
 
     if not moved:
         player_car.reduce_speed()
+
 
 
 pygame.quit()
